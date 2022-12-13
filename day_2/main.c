74b43abbc50cf1d141f6fa0a3e9ac79135d6154f
DennisLovkvist <dennis.lovkvist@protonmail.com>
2022-12-13 01:23:59 +0100

    solved part 2.

--- day_2/main.c
+++ day_2/main.c
@@ -15,7 +15,7 @@ static char* load_input(char *path)
     source_code[fsize] = '\0';
     return source_code;
 }
-int main()
+void part1()
 {
     char* input_raw = load_input("input.txt");
     int length = strlen(input_raw);
@@ -56,6 +56,56 @@ int main()
     }
     
     printf("%s%i\n","points: ", points);
+}
 
+void part2()
+{
+    char* input_raw = load_input("input.txt");
+    int length = strlen(input_raw);
+
+    int outcome = 0;
+    int opponent = 0;
+    int points = 0;
+
+    for (size_t i = 0; i < length; i+=4)
+    {
+        
+        opponent = input_raw[i] - 64;
+        outcome = input_raw[i+2]-23- 64;
+
+        int me = 0;
+        int o1 = (outcome == 1);
+        me += o1 * ((opponent == 1) * 3);
+        me += o1 * ((opponent == 2) * 1);
+        me += o1 * ((opponent == 3) * 2);
+
+        me += (outcome == 2) * opponent;
+
+        int o2 = (outcome == 3);
+        me += o2 * ((opponent == 1) * 2);
+        me += o2 * ((opponent == 2) * 3);
+        me += o2 * ((opponent == 3) * 1);
+
+        points += me;
+
+        int g1 = ((opponent == 1)*(me == 2));
+        int g2 = ((opponent == 2)*(me == 3));
+        int g3 = ((opponent == 3)*(me == 1));
+        int g4 = (opponent == me);
+
+        points += ((6+me) * g1) - (me*g1);
+        points += ((6+me) * g2) - (me*g2);
+        points += ((6+me) * g3) - (me*g3);
+        points += ((3+me) * g4) - (me*g4);
+    }
+    
+    printf("%s%i\n","points: ", points);
+}
+int main()
+{
+
+    //part1();
+    
+    part2();
     return 1;
 }
