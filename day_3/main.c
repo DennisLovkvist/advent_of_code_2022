861be4ebccd935d92bbbf3ae5d0485cf183b6672
DennisLovkvist <dennis.lovkvist@protonmail.com>
2022-12-13 02:06:17 +0100

    solved day 3 part 1

---
+++ day_3/main.c
@@ -0,0 +1,73 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char* load_input(char *path)
+{
+    FILE *f = fopen(path, "rb");
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    char *source_code = malloc(fsize + sizeof(char));
+    fread(source_code, fsize, 1, f);
+    fclose(f);
+    source_code[fsize] = '\0';
+    return source_code;
+}
+void part1()
+{
+    char* input_raw = load_input("input.txt");
+    int length = strlen(input_raw);
+
+    int count = 0;
+    int start = 0;
+    int sum = 0;
+    for (size_t i = 0; i < length+1; i++)
+    {
+        if(input_raw[i] == '\n' || i == length)
+        {
+            //alloc on stack
+            char *line = alloca(sizeof(char)*count+1);
+            memcpy(line,&input_raw[start],sizeof(char)*count);
+            line[count] = '\0';
+
+            int middle = count/2;
+            for (size_t j = 0; j < middle; j++)
+            {
+                for (size_t k = middle; k < count; k++)
+                {
+                    if(line[j] == line[k])
+                    {
+                        int c = line[j];
+                        if(c > 96 && c < 123)
+                        {
+                            c-=96;
+                        }
+                        else
+                        {
+                            c-=38;
+                        }
+                        sum += c;
+                        goto end_loop;
+                    }
+                }
+            }
+            end_loop:
+            start += count+1;
+            count = 0;
+        }
+        else
+        {
+            count ++;
+        }
+    }
+
+    printf("%s%i","sum of the priorities: ", sum);
+}
+int main()
+{
+    part1();
+    
+    return 1;
+}
