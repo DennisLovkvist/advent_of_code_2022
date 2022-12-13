29e19f468292da8b1fec164c8edfa345e18290f0
DennisLovkvist <dennis.lovkvist@protonmail.com>
2022-12-13 20:24:13 +0100

    solved day 4 part 2

--- day_4/main.c
+++ day_4/main.c
@@ -18,7 +18,7 @@ static char* load_input(char *path)
 }
 void part1()
 {
-    char* input_raw = load_input("input_part_1.txt");
+    char* input_raw = load_input("input.txt");
     int length = strlen(input_raw);
 
     int groups[4];
@@ -51,9 +51,45 @@ void part1()
     }
     printf("%i%s", sum, " pairs contain eachother");
 }
+void part2()
+{
+    char* input_raw = load_input("input.txt");
+    int length = strlen(input_raw);
+
+    int groups[4];
+    int index = 0,sum = 0,count = 0;
+    char digits[3] = {'\0','\0','\0'};
+
+    for (size_t i = 0; i < length+1; i++)
+    {
+        char c = input_raw[i];
+        int line_feed = (c == '\n'|| i == length);
+        int delimiter = (c == '-' || c == ',');
+        
+        if(line_feed || delimiter)
+        {
+            groups[index] = atoi(digits);
+            digits[0] = digits[1] = '\0';
+            index ++;
+            count = 0;
+        
+            if(line_feed)
+            {
+                index = 0;
+                sum += ((groups[0] <= groups[2]) * (groups[2] <= groups[1])) || ((groups[2] <= groups[0]) * (groups[0] <= groups[3]));//void part2() is the same as void part1() except this line
+            } 
+        }
+        else
+        {
+            digits[count++] = c;                
+        }  
+    }
+    printf("%i%s", sum, " pairs overlaps");
+}
 int main()
 {
-    part1();
+    //part1();
+    part2();
     return 1;
 }
 
