ccbfff8ffd2f6f98628bb1a4735668310474424e
DennisLovkvist <dennis.lovkvist@protonmail.com>
2022-12-20 01:28:35 +0100

    parsing done

--- day_13/main.c
+++ day_13/main.c
@@ -8,13 +8,23 @@
 #include <string.h>
 #include <unistd.h>
 
-typedef struct Node Node;
-struct Node
-{
-    Node *parent;
-    int index;
-    
+#define ELEMENT_TYPE_UNKNOWN 0
+#define ELEMENT_TYPE_LIST 1
+#define ELEMENT_TYPE_DIGIT 2
 
+
+typedef struct Element Element;
+struct Element
+{
+    Element *parent;
+    Element **children;
+    int child_count;
+    unsigned int type;
+    int* list;
+    int digit;
+    int depth;
+    int index;
+    int list_index;
 };
 
 static char* load_input(char *path)
@@ -29,24 +39,18 @@ static char* load_input(char *path)
     source_code[fsize] = '\0';
     return source_code;
 }
-int find_closing(char *line, int length, int index)
+Element *new_element(int depth)
 {
-    for (size_t i = index; i < length; i++)
-    {
-        if(line[i] == ']')
-        {
-            return i;
-        }
-        if(line[i] == '[')
-        {
-            int closing = find_closing(line,length,i);
-            int *list = parse
-        }
-    }
-
-
-    return 0;
-    
+    Element *e = malloc(sizeof(Element));
+    e->depth = depth;
+    e->type = ELEMENT_TYPE_UNKNOWN;
+    e->digit = 0;
+    e->list_index = 0;
+    e->index = 0;
+    e->child_count = 0;
+    e->children = malloc(0);
+    e->parent = NULL;
+    return e;
 }
 int parse_digit(char *line, int s, int e)
 {
@@ -56,65 +60,125 @@ int parse_digit(char *line, int s, int e)
     digit[length] = '\0';
     return atoi(digit);
 }
-void append_digit(Node *parent, int digit)
+void append_child(Element *parent,Element  *child)
 {
-
+    parent->child_count ++;
+    parent->children = realloc(&parent->children[0],sizeof(Element*)*parent->child_count);
+    parent->children[parent->child_count-1] = child;
 }
-int find_children(Node *parent,char *line)
+Element *parse(char *line)
 {
+    printf("%s\n",line);
     int index = 0;
     char c = line[index];
-    int digit_start = 0,digit_end = 0;
-    unsigned int digit_begun = 0;
+
+    int opened = 1;
+    int closed = 0;
+    int depth = 1;
+
+    Element *root = new_element(0);
+    root->type = ELEMENT_TYPE_LIST;
+    Element *current_element = root;
+    root->parent = NULL;
+
+    int digit_new = 1,digit_start = 0,digit_end = 0;
+
     while(c != '\0')
     {
-        if(c == '[')
+        if(c == ']')
         {
-             Node *node = malloc(sizeof(Node));
-            find_children(node,&line[index]);
-        }
-        else if(c == ']')
-        {
-            if(digit_begun)
+            if(current_element->type == ELEMENT_TYPE_DIGIT)
             {
                 digit_end = index;
                 int digit = parse_digit(line,digit_start,digit_end);
-                append_digit(parent,digit);
-                digit_begun = 0;
+                current_element->digit = digit;
+                append_child(current_element->parent,current_element);
+                current_element = current_element->parent;
+                digit_new = 1;
+                //parse digit
             }
-            return index;
+
+            closed ++;
+            depth--;
+            current_element->type = ELEMENT_TYPE_LIST;
+            if(current_element->parent != NULL)
+            {
+                append_child(current_element->parent,current_element);
+                current_element = current_element->parent;
+            }
+            
+        }
+        else if(c == '[')
+        {
+            Element *e = new_element(depth);
+            e->type = ELEMENT_TYPE_LIST;
+            e->parent = current_element;
+            current_element = e;
+            opened ++;
+            depth++;
         }
         else if(c == ',')
         {
-            digit_end = index;
-            int digit = parse_digit(line,digit_start,digit_end);
-            append_digit(parent,digit);
-            digit_begun = 0;
+            if(current_element->type == ELEMENT_TYPE_DIGIT)
+            {
+                digit_end = index;
+                int digit = parse_digit(line,digit_start,digit_end);
+                current_element->digit = digit;
+                append_child(current_element->parent,current_element);
+                current_element = current_element->parent;
+                digit_new = 1;
+                current_element->list_index++;
+            }
+            else if(current_element->type == ELEMENT_TYPE_LIST)
+            {
+                append_child(current_element->parent,current_element);
+                current_element->list_index++;
+            }
+
+            
+            
         }
         else
         {
-            if(!digit_begun)
+            if(digit_new)
             {
-                digit_begun = 1;
+                Element *e = new_element(depth);//create new element
+                e->type = ELEMENT_TYPE_DIGIT;
+                e->parent = current_element;//set parent to the current element
+                e->index = current_element->list_index;//set the elements index as the current list index in the parent
+                current_element->list_index++;//increment the parents list index
+                current_element = e;//switch to elment as the current element
                 digit_start = index;
+                digit_new = 0;
             }
         }
-
-
         index++;
         c = line[index];
     }
-    return -1;
+    return root;
 }
-void parse(char *line, int length)
+void  debug_print_element_tree(Element *e )
 {
+    printf("%s","[");
+    for (size_t i = 0; i < e->child_count; i++)
+    {
+        if(e->children[i]->type == ELEMENT_TYPE_DIGIT)
+        {
+            printf("%i",e->children[i]->digit);
+            
+        }
+        else
+        {
+            debug_print_element_tree(e->children[i]);
+        }
+        if(i != e->child_count-1)
+        {
+            printf(",");
+        }
+    }
+    printf("%s","]");
     
-    Node *root = malloc(sizeof(Node));
-
-    find_children(root,&line[1]);
-
-    
-}
+}         
 int main()
 {
     srand(time(NULL));
@@ -130,15 +194,25 @@ int main()
         if(c == '\n' || c == '\0')
         {
             int len = index -start;
-            char *line = alloca(sizeof(char)*(len+1));
-            memcpy(line,&input_raw[start],sizeof(char)*len);
-            line[len] = '\0';
+            if(len > 0)
+            {
+                char *line = alloca(sizeof(char)*(len+1));
+                memcpy(line,&input_raw[start],sizeof(char)*len);
+                line[len] = '\0';
 
-            parse(line,len);
+                Element *e = parse(line);
+
+                debug_print_element_tree(e->children[0]);
+                printf("\n");
 
 
-            start = index +1;
+            // printf("%i\n",e->child_count);
+                
+            }
+
+        start = index +1;
         }
+        
         index++;
     }
 
