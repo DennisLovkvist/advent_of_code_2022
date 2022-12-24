e415e03536de2e9cf98da97efcd80e84d865228b
DennisLovkvist <dennis.lovkvist@protonmail.com>
2022-12-25 00:26:53 +0100

    solved part 2

--- day_15/main.c
+++ day_15/main.c
@@ -75,9 +75,7 @@ int distance_manhattan(int x1, int y1, int x2, int y2)
 {
    return abs(x2-x1)+abs(y2-y1);
 }
-#define Y 2000000
-//#define Y 10
-int main()
+void part1()
 {
     char* input_raw = load_input("input.txt");
     int length = strlen(input_raw);
@@ -128,55 +126,52 @@ int main()
         index++;
     }
 
-    Vector2i *ranges = malloc(sizeof(Vector2i)*line_count);
-    int ranges_index = 0;
+    Vector2i *covered_ranges = malloc(sizeof(Vector2i)*line_count);
+    int covered_ranges_count = 0;
 
     
     for (size_t i = 0; i < line_count; i++)
     {
-        Vector2i b = beacons[i];
-        Vector2i s = sensors[i];
+        Vector2i beacon = beacons[i];
+        Vector2i sensor = sensors[i];
 
 
-        int dist_s_b = distance_manhattan(s.x,s.y,b.x,b.y);
-        int dist_s_y = distance_manhattan(s.x,s.y,s.x,Y);
+        int dist_sensor_to_beacon = distance_manhattan(sensor.x,sensor.y,beacon.x,beacon.y);
+        int dist_sensor_to_y = distance_manhattan(sensor.x,sensor.y,sensor.x,2000000);
 
-        if(dist_s_y < dist_s_b)
+        if(dist_sensor_to_y < dist_sensor_to_beacon)
         {
-            int lol = dist_s_b-dist_s_y;
-
-            
-            int min = s.x - lol;
-        
-            int max = s.x+lol;
+            int n = dist_sensor_to_beacon - dist_sensor_to_y;
+            int min = sensor.x - n;
+            int max = sensor.x + n;
 
             unsigned int flag = 0;
 
-            for (size_t j = 0; j < ranges_index; j++)
+            for (size_t j = 0; j < covered_ranges_count; j++)
             {
-                if(ranges[j].x <= min && ranges[j].y >= max)
+                if(covered_ranges[j].x <= min && covered_ranges[j].y >= max)
                 {
                     flag = 1;
                     break;
                 }
-                else if(min <= ranges[j].x && max >= ranges[j].y)
+                else if(min <= covered_ranges[j].x && max >= covered_ranges[j].y)
                 {
-                    ranges[j].x = min;
-                    ranges[j].y = max;
+                    covered_ranges[j].x = min;
+                    covered_ranges[j].y = max;
                     flag = 1;
                     break;
                 }
                 else
                 {
-                    if(min < ranges[j].x && max < ranges[j].y)
+                    if(min < covered_ranges[j].x && max < covered_ranges[j].y)
                     {
-                        ranges[j].x = min;
+                        covered_ranges[j].x = min;
                         flag = 1;
                         break;
                     }
-                    else if(max > ranges[j].y && min > ranges[j].x)
+                    else if(max > covered_ranges[j].y && min > covered_ranges[j].x)
                     {
-                        ranges[j].y = max;
+                        covered_ranges[j].y = max;
                         flag = 1;
                         break;
                     }
@@ -185,9 +180,9 @@ int main()
 
             if(!flag)
             {
-                ranges[ranges_index].x = min;
-                ranges[ranges_index].y = max;
-                ranges_index++;
+                covered_ranges[covered_ranges_count].x = min;
+                covered_ranges[covered_ranges_count].y = max;
+                covered_ranges_count++;
             }
             
         }
@@ -195,21 +190,120 @@ int main()
 
         
     }
-  
-  for (size_t j = 0; j < ranges_index; j++)
+    for (size_t j = 0; j < covered_ranges_count; j++)
     {
-        printf("%i\n",ranges[j].y-ranges[j].x);
+        printf("%i\n",covered_ranges[j].y-covered_ranges[j].x);
 
     }
+}
+void part2()
+{
+    char* input_raw = load_input("input.txt");
+    int length = strlen(input_raw);
 
+    int index = 0;
+    int start = 0;
+    int count = 0;
+
+    int line_count = 0;
+
+    while(index <= length)
+    {
+        char c = input_raw[index];
+        if(c == '\n' || c == '\0')
+        {
+            line_count ++;
+        }
+        index++;
+    }
     
 
+    Vector2i *sensors = malloc(sizeof(Vector2i)*line_count);
+    Vector2i *beacons = malloc(sizeof(Vector2i)*line_count);
+
+    index = 0;
+    line_count = 0;
+
+    while(index <= length)
+    {
+        char c = input_raw[index];
+        if(c == '\n' || c == '\0')
+        {
+            int len = index -start;
+            if(len > 0)
+            {
+                char *line = alloca(sizeof(char)*(len+1));
+                memcpy(line,&input_raw[start],sizeof(char)*len);
+                line[len] = '\0';
 
 
-    
-    
-    
+                parse_line(line,&sensors[line_count],&beacons[line_count]);
+                line_count++;
+            }
 
+            start = index +1;
+        }
+        
+        index++;
+    }
+
+    Vector2i *covered_range = malloc(sizeof(Vector2i)*line_count);
+    
+    #define FOUR_MILLION 4000000
+    
+    for (int  i = 0; i < FOUR_MILLION; i++)
+    {
+        int covered_ranges_count = 0;
+
+        for (size_t j = 0; j < line_count; j++)
+        {
+            Vector2i beacon = beacons[j];
+            Vector2i sensor = sensors[j];
+
+            int dist_sensor_to_beacon = distance_manhattan(sensor.x,sensor.y,beacon.x,beacon.y);
+            int dist_sensor_to_y = distance_manhattan(sensor.x,sensor.y,sensor.x,i);
+
+            if(dist_sensor_to_y <= dist_sensor_to_beacon)
+            {
+                int n = dist_sensor_to_beacon - dist_sensor_to_y;
+                int min = sensor.x - n;
+                int max = sensor.x + n;
+                
+                covered_range[covered_ranges_count].x = min;
+                covered_range[covered_ranges_count].y = max;
+                covered_ranges_count++;
+            }
+        }
+
+        for (int j = 0; j < FOUR_MILLION; j++)
+        {
+            unsigned int within_range = 0;
+            for (int l = 0; l < covered_ranges_count; l++)
+            {
+                if(j >= covered_range[l].x && j <= covered_range[l].y)
+                {
+                    j = covered_range[l].y;
+                    within_range = 1;
+                    goto end;
+                }
+            }
+
+            end:
+            if(!within_range)
+            {
+                unsigned long x = (unsigned long)j;
+                unsigned long y = (unsigned long)i;
+                unsigned long four_million = FOUR_MILLION;
+                unsigned long tuning_frequency = x * four_million + y;
+                printf("tuning frequency: %lu\n",tuning_frequency);
+                return;
+            }
+        }
+    }
+}
+int main()
+{
+    part1();
+    part2();
     return 1;
 }
-
